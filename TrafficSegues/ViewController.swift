156ac2a914d5c69d813136155e08b6ca208c9276
Jeff Anderson <jeff.anderson867@gmail.com>
2017-06-02 16:04:17 -0400

    basic app practice on view segues

--- TrafficSegues/ViewController.swift
+++ TrafficSegues/ViewController.swift
@@ -9,17 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var redToggle: UISwitch!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+  
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    @IBAction func yellowButtonTapped(_ sender: Any) {
+        if redToggle.isOn{
+            performSegue(withIdentifier: "Yellow", sender: nil)
+        }
+    }
+    @IBAction func greenButtonTapped(_ sender: Any) {
+        if redToggle.isOn{
+            performSegue(withIdentifier: "Green", sender: nil)
+        }
 
-
+    
+    }
+  
 }
 
